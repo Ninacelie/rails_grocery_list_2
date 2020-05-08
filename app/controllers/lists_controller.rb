f26614bc589dbdedb65ec817e57c3b29fd053e61
Nina Angelo <ninacelie@gmail.com>
2020-05-07 21:51:18 -0500

    updates routes and lists controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,37 @@
 class ListsController < ApplicationController
+  before_action :redirect_if_not_logged_in 
+
+  def new
+    @list = List.new
+  end 
+
+  def create
+    @user = current_user
+    list_fields = list_params 
+    list_fields[:user_id] = @user.id
+    @list = List.new(list_fields)
+    if @list.save 
+      redirect_to "/users/#{@user.id}" #users home page 
+    else 
+      render "/signup" 
+    end 
+  end 
+
+  def update
+    @list = List.find(params[:id])
+    @list.update(title: params[:list][:title], content: params [:list][:content])
+    redirect_to "/lists"
+  end 
+
+  def show 
+    list_id = params[:id]
+    @list = List.find(list_id)
+  end
+
+  private 
+
+  def list_params
+    params.require(:list).permit(:title, :content)
+  end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,31 @@
 Rails.application.routes.draw do
+  root 'sessions#home'
+  
+  #signup routes
+  get '/signup', to: 'users#new' 
+  # post '/users', to: 'users#create'
+
+  #login routes
+  get '/login', to: 'sessions#new'
+  post '/sessions', to: 'sessions#create'
+
+  #logout routes
+  post '/logout', to: 'sessions#logout'
+
+  # omniauth
+  get '/auth/:provider/callback', to: 'sessions#create_from_omniauth'
 
   resources :users do 
     resources :lists, only: [:new,:create,:index] # shallow routing 
   end 
-  
+
   resources :lists do
     resources :items, only: [:new, :create, :index]
   end 
 
-  resources :items
-  resources :list_items
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/completed', to: 'items#completed'
+
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
+
