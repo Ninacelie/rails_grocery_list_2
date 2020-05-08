384ab1f1b336946d4e18962359d0de06a2d0b024
Nina Angelo <ninacelie@gmail.com>
2020-05-07 21:06:01 -0500

    adds routes

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+
+  has_many :list_items
+  has_many :lists, through: :list_items
+
+  validates :content, presence: :true 
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  resources :list_items
+
+  resources :users do 
+    resources :lists, only: [:new,:create,:index] # shallow routing 
+  end 
+  
+  resources :lists do
+    resources :items, only: [:new, :create, :index]
+  end 
+
   resources :items
-  resources :lists
-  resources :users
+  resources :list_items
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
